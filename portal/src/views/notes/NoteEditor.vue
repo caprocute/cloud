6d6971716bae66cde0c7edad384b4b538bea4741
Andrei Radoi <andrei.radoi@neurony.ro>
2023-09-25 20:05:34 +0300

    GL-16 fixes

--- portal/src/views/notes/NoteEditor.vue
+++ portal/src/views/notes/NoteEditor.vue
@@ -3,7 +3,9 @@
         <div class="title">
             <TextAreaField v-if="editingTitle" v-model="title" />
             <template v-else>{{ title }}</template>
-            <a class="edit-btn" v-if="editableTitle && !editingTitle" @click="editingTitle = !editingTitle">{{ $t("notes.customTitleEditLabel") }}</a>
+            <a class="edit-btn" v-if="editableTitle && !editingTitle" @click="editingTitle = !editingTitle">
+                {{ $t("notes.customTitleEditLabel") }}
+            </a>
         </div>
         <div class="field" v-if="!readonly">
             <TextAreaField v-model="body" @input="v.$touch()" />
@@ -72,6 +74,9 @@ export default Vue.extend({
         },
         title: {
             get(this: any) {
+                if (this.editingTitle) {
+                    return this.note.title;
+                }
                 return this.note.title || this.note.help.title;
             },
             set(this: any, value) {
