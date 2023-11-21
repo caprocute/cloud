6a691ca18e0aa9db36662fda5048df85fbf1d36c
Andrei Radoi <andrei.radoi@neurony.ro>
2023-11-21 17:28:15 +0200

    share cypress test stationPageUrl variable

--- portal/cypress/e2e/spec.cy.ts
+++ portal/cypress/e2e/spec.cy.ts
@@ -1,12 +1,8 @@
 import "cypress/support/commands";
-import {ActionTypes} from "../../src/store";
-import {SnackbarStyle} from "../../src/store/modules/snackbar";
-import {
-    getPartnerCustomizationWithDefault,
-    isCustomisationEnabled,
-    PartnerCustomization
-} from "../../src/views/shared/partners";
-import {apiUrl} from '../support/commands';
+import { ActionTypes } from "../../src/store";
+import { SnackbarStyle } from "../../src/store/modules/snackbar";
+import { getPartnerCustomizationWithDefault, isCustomisationEnabled, PartnerCustomization } from "../../src/views/shared/partners";
+import { apiUrl } from "../support/commands";
 
 //import {FKApi} from '../../src/api';
 
@@ -14,81 +10,75 @@ const stationPageUrl = "/station/1";
 
 describe("Station Page", () => {
 
-    it("should create a new station and navigate to its page", () => {
+    beforeEach(() => {
         cy.login();
         cy.addStation();
-        cy.get('@stationPageUrl').then((stationPageUrl) => {
-            cy.wrap(stationPageUrl).as('stationPageUrl');
-            Cypress.env('stationPageUrl', stationPageUrl);
-            cy.visit(stationPageUrl);
+        cy.get("@stationPageUrl").then((stationPageUrl) => {
+            cy.wrap(stationPageUrl).as("stationPageUrl");
+            Cypress.env("stationPageUrl", stationPageUrl);
         });
     });
 
-   it("shows station details form", () => {
-       cy.get('@stationPageUrl').then((stationPageUrl) => {
-           cy.visit(stationPageUrl);
-           cy.get('[data-cy="notesForm"]');
-       });
-   });
-    /*
-      it("should display save button if user is authenticated", () => {
-          cy.login();
-          cy.visit(stationPageUrl);
-          cy.get('[data-cy="saveNotes"]');
-      });
+    it("should create a new station and navigate to its page", () => {
+        cy.visit("@stationPageUrl");
+    });
 
-      it("should not display save button if user is not authenticated", () => {
-          cy.login();
-          cy.visit(stationPageUrl);
-          cy.get('[data-cy="saveNotes"]').should("not.exist");
-      });
+    it("should display save button if user is authenticated", () => {
+        cy.login();
+        cy.visit(stationPageUrl);
+        cy.get('[data-cy="saveNotes"]');
+    });
 
-      it("should successfully save the form when valid data is entered", () => {
-          // Assuming that there are commands to log in a user and set up necessary preconditions.
-          cy.login();
+    it("should not display save button if user is not authenticated", () => {
+        cy.login();
+        cy.visit(stationPageUrl);
+        cy.get('[data-cy="saveNotes"]').should("not.exist");
+    });
 
-          cy.wait(2000);
+   /* it("should successfully save the form when valid data is entered", () => {
+        // Assuming that there are commands to log in a user and set up necessary preconditions.
+        cy.login();
 
-          cy.visit(stationPageUrl);
+        cy.wait(2000);
 
-          cy.get('[data-cy="studyObjectiveBody"]').type("Some text");
-          cy.get('[data-cy="sitePurposeBody"]').type("Some text");
-          cy.get('[data-cy="siteCriteriaBody"]').type("Some text");
-          cy.get('[data-cy="siteDescriptionBody"]').type("Some text");
-          cy.get('[data-cy="customKeyBody"]').type("Some text");
+        cy.visit(stationPageUrl);
 
-          cy.get('[data-cy="editCustomKey"]').click();
-          cy.get('[data-cy="customKeyTitle"]').clear().type("Some title");
+        cy.get('[data-cy="studyObjectiveBody"]').type("Some text");
+        cy.get('[data-cy="sitePurposeBody"]').type("Some text");
+        cy.get('[data-cy="siteCriteriaBody"]').type("Some text");
+        cy.get('[data-cy="siteDescriptionBody"]').type("Some text");
+        cy.get('[data-cy="customKeyBody"]').type("Some text");
 
-          // Stub response for API call(s) made during form submission (replace '/api-endpoint' with actual endpoint)
-          cy.intercept("PATCH", "/stations/1/notes", {fixture: "success.json"}).as("submitForm");
+        cy.get('[data-cy="editCustomKey"]').click();
+        cy.get('[data-cy="customKeyTitle"]').clear().type("Some title");
 
-          // Click submit button
-          cy.get('.buttons button[type="submit"]').click();
+        // Stub response for API call(s) made during form submission (replace '/api-endpoint' with actual endpoint)
+        cy.intercept("PATCH", "/stations/1/notes", { fixture: "success.json" }).as("submitForm");
 
-          // Assert that API call was successful
-          cy.wait("@submitForm").its("response.statusCode").should("eq", 200);
-      });
+        // Click submit button
+        cy.get('.buttons button[type="submit"]').click();
 
-      it("go back to stations dashboard", () => {
-          cy.login();
-          cy.visit(stationPageUrl);
-          cy.get('[data-cy="backBtn"]').click();
-          cy.url().should('eq', Cypress.config('baseUrl') + '/dashboard/stations/1');
-      });
+        // Assert that API call was successful
+        cy.wait("@submitForm").its("response.statusCode").should("eq", 200);
+    });
 
-      it("shows Field Notes Section", () => {
-          cy.login();
-          cy.visit(stationPageUrl);
-          cy.get('[data-cy="fieldNotes"]');
-      });
+    it("go back to stations dashboard", () => {
+        cy.login();
+        cy.visit(stationPageUrl);
+        cy.get('[data-cy="backBtn"]').click();
+        cy.url().should("eq", Cypress.config("baseUrl") + "/dashboard/stations/1");
+    });
 
-      it("can edit station description", () => {
+    it("shows Field Notes Section", () => {
+        cy.login();
+        cy.visit(stationPageUrl);
+        cy.get('[data-cy="fieldNotes"]');
+    });
 
-          const partnerCustomization = window.location.hostname.indexOf("floodnet.") >= 0;
+    it("can edit station description", () => {
+        const partnerCustomization = window.location.hostname.indexOf("floodnet.") >= 0;
 
-          if (!partnerCustomization) {
-
-          }
-      });*/
+        if (!partnerCustomization) {
+        }
+    });*/
 });
