e62706639c398ed3d9f6f0af1e0ce103122948bf
Jacob Lewallen <jlewallen@gmail.com>
2024-01-02 15:03:50 -0800

    Module ids are mapped based on last sensor reading.
    When we added associated stations and the ability to combine/merge
    sensors for them, this code to remap module IDs back to the original
    station was added. This code breaks when a module moves from one station
    to another and both stations are present in data viz.
    
    So, as a quick solution we sort by the sensor reading date. I
    believe this can and should be improved further. For example,
    the instances where this remapping happens could probably be narrowed
    significantly.

--- portal/src/views/viz/viz.ts
+++ portal/src/views/viz/viz.ts
@@ -866,17 +866,32 @@ export class Workspace implements VizInfoFactory {
     }
 
     private mapModulesToStations() {
+        // Right now we sort this list by sensor reading time so the most recent
+        // station that had a module gets returned. This is primarily for
+        // remapping modules from associated stations back to the original
+        // station, and could definitely be improved. See
+        // `dereferenceAssociatedVizQuery`
         return _(Object.values(this.stations))
-            .map((stationMeta) => stationMeta.sensors.map((sensorMeta) => [sensorMeta.moduleId, stationMeta.id]))
+            .map((stationMeta) =>
+                _(stationMeta.sensors)
+                    .map((sensorMeta) => {
+                        return { order: new Date(sensorMeta.sensorReadAt), row: [sensorMeta.moduleId, stationMeta.id] };
+                    })
+                    .value()
+            )
             .flatten()
+            .orderBy((ordered) => ordered.order)
+            .map((ordered) => ordered.row)
             .fromPairs()
             .value();
     }
 
     private dereferenceAssociatedVizQuery(vq: VizQuery): VizQuery {
+        // We can probably get away with leaving queries alone in many situations.
         const modulesToStations = this.mapModulesToStations();
-        // console.log("viz: dereference", vq.params.sensors, modulesToStations);
-        return vq.remapStationsFromModules(modulesToStations);
+        const after = vq.remapStationsFromModules(modulesToStations);
+        console.log("viz: dereference", vq.params.sensors, modulesToStations, vq.params.stations, "->", after.params.stations);
+        return after;
     }
 
     public availableChartTypes(viz: Graph, ds: DataSetSeries | undefined = undefined): ChartType[] {
