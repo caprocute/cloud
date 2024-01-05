aa54cc076e86cb04dd1285cc223da967f7419677
Jacob Lewallen <jlewallen@gmail.com>
2024-01-05 09:38:23 -0800

    Copy env and dump schema only as test.

--- tools/db-clone.sh
+++ tools/db-clone.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export TERRAFORM_ENV=env.json
+
 # We need the environment.
 ENV=$1
 
@@ -8,8 +10,20 @@ if [ -z "$ENV" ]; then
 	exit 2
 fi
 
+# Change to script directory.
+cd "${0%/*}"
+echo `pwd`
+
+STAMP=`date +%Y%m%d_%H%M%S`
+FILE=db-${ENV}-${STAMP}.sql
+echo ${FILE}
+
 # Warning: If you enable set -x then you will leak database passwords.
 set -e
 
+# Warning: If you enable set -x then you will leak database passwords.
+DATABASE_URL=`jq -r .database_url.value $TERRAFORM_ENV` docker run --log-driver none --rm postgres pg_dump --schema-only "${DATABASE_URL}" > ${FILE}
+
+# docker run --log-driver none --rm postgres pg_dump --data-only --disable-triggers ${extraArgs} '${url}' >> ${FILE}
 
 echo done
