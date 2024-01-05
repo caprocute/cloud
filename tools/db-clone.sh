c887dbe98354193c5de635c186b8bdd778c4db2e
Jacob Lewallen <jlewallen@gmail.com>
2024-01-05 14:23:09 -0800

    Export tables, compress, send to syncfolder.

--- tools/db-clone.sh
+++ tools/db-clone.sh
@@ -2,14 +2,6 @@
 
 export TERRAFORM_ENV=env.json
 
-# We need the environment.
-ENV=$1
-
-if [ -z "$ENV" ]; then
-	echo "usage: db-clone.sh ENV"
-	exit 2
-fi
-
 # Change to script directory.
 cd "${0%/*}"
 echo `pwd`
@@ -21,9 +13,39 @@ echo ${FILE}
 # Warning: If you enable set -x then you will leak database passwords.
 set -e
 
-# Warning: If you enable set -x then you will leak database passwords.
-DATABASE_URL=`jq -r .database_url.value $TERRAFORM_ENV` docker run --log-driver none --rm postgres pg_dump --schema-only "${DATABASE_URL}" > ${FILE}
+ls  -alh
 
-# docker run --log-driver none --rm postgres pg_dump --data-only --disable-triggers ${extraArgs} '${url}' >> ${FILE}
+# Warning: If you enable set -x then you will leak database passwords.
+DATABASE_URL=`jq -r .database_url.value $TERRAFORM_ENV`
+
+echo Exporting...
+
+# Schema first.
+docker run --log-driver none --rm postgres pg_dump --schema-only "${DATABASE_URL}" > ${FILE}
+
+# Users table, sanitized of passwords.
+docker run --log-driver none --rm postgres pg_dump --data-only --disable-triggers ${extraArgs} "${DATABASE_URL}" \
+        -t fieldkit.user | ./desecreter >> ${FILE}
+
+# Everything else, exluding heavy and obsolete tables, as well as user which we've already done.
+docker run --log-driver none --rm postgres pg_dump --data-only --disable-triggers ${extraArgs} "${DATABASE_URL}" \
+        -T fieldkit.user \
+        -T fieldkit.aggregated_24h \
+        -T fieldkit.aggregated_12h \
+        -T fieldkit.aggregated_6h \
+        -T fieldkit.aggregated_1h \
+        -T fieldkit.aggregated_30m \
+        -T fieldkit.aggregated_10m \
+        -T fieldkit.aggregated_1m \
+        -T fieldkit.aggregated_10s \
+        -T fieldkit.ttn_messages \
+        -T fieldkit.bookmarks \
+        -T fieldkit.ingestion_queue \
+        -T fieldkit.data_record >> ${FILE}
+
+# Compress and send to the sync folder.
+echo Compressing...
+bzip2  ${FILE}
+mv ${FILE}.bz2 /svr0/synced/dbs
 
 echo done
