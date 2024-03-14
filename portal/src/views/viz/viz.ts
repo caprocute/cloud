6e0ee431c387c4560023e2d3054ea206d2e457d6
Andrei Radoi <andrei.radoi@neurony.ro>
2024-03-14 21:04:46 +0200

    fix data view axis translate

--- portal/src/views/viz/viz.ts
+++ portal/src/views/viz/viz.ts
@@ -28,16 +28,16 @@ import { promiseAfter } from "@/utilities";
 import { createSensorColorScale } from "./d3-helpers";
 import { DisplayStation } from "@/store";
 import { getPartnerCustomizationWithDefault } from "../shared/partners";
-import {Locales} from '@/views/shared/LanguageSelector.vue';
+import { Locales } from "@/views/shared/LanguageSelector.vue";
 
 export * from "./common";
 
 type SensorReadAtType = string;
 
-const locale = localStorage.getItem("locale") as Locales;
+const localeKey = (localStorage.getItem("locale") as Locales).replace("-", "");
 
 function getString(d) {
-    return d[locale] || d["enUS"] || d["enUs"] || d["en-US"]; // HACK
+    return d[localeKey] || d["enUS"] || d["enUs"] || d["en-US"]; // HACK
 }
 
 function getBackend(): string | null {
