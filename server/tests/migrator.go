6ef6dc91978f1d4af8213dc4a5b6a2d1ff9ed449
Jacob Lewallen <jlewallen@gmail.com>
2024-08-13 14:54:06 -0700

    Single migration helper, avoid future issues.

--- server/tests/migrator.go
+++ server/tests/migrator.go
@@ -1,17 +1,12 @@
 package tests
 
 import (
-	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 
-	"github.com/go-pg/pg/v10"
-	"github.com/go-pg/pg/v10/orm"
-
-	migrations "github.com/robinjoseph08/go-pg-migrations/v3"
+	support "gitlab.com/fieldkit/cloud/migrations/support"
 )
 
 var (
@@ -19,112 +14,33 @@ var (
 )
 
 func tryMigrate(url string) error {
-	migrationsDir, err := findMigrationsDirectory("primary")
+	path, err := findMigrationsPath("primary")
 	if err != nil {
 		return err
 	}
 
 	log.Printf("trying to migrate...")
 	log.Printf("postgres = %s", url)
-	log.Printf("migrations = %s", migrationsDir)
+	log.Printf("migrations = %s", path)
 
-	files, err := filepath.Glob(filepath.Join(migrationsDir, "*.up.sql"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	migrator := support.NewMigrator(url, path)
 
 	if !registered {
-		for _, file := range files {
-			data, err := ioutil.ReadFile(file)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			text := string(data)
-
-			up := func(db orm.DB) error {
-				_, err := db.Exec(text)
-				return err
-			}
-
-			down := func(db orm.DB) error {
-				return err
-			}
-
-			opts := migrations.MigrationOptions{}
-
-			_, fileOnly := filepath.Split(file)
-
-			migrations.Register(fileOnly, up, down, opts)
+		if err := migrator.Load(); err != nil {
+			return err
 		}
 
 		registered = true
 	}
 
-	options, err := pg.ParseURL(url)
-	if err != nil {
+	if err := migrator.Run([]string{"", "migrate"}); err != nil {
 		return err
-	}
-
-	options.OnConnect = func(ctx context.Context, conn *pg.Conn) error {
-		log.Printf("creating schema...")
-
-		if _, err := conn.Exec("CREATE SCHEMA IF NOT EXISTS fieldkit"); err != nil {
-			return fmt.Errorf("error creating: %w", err)
-		}
-
-		if _, err := conn.Exec("GRANT USAGE ON SCHEMA fieldkit TO fieldkit"); err != nil {
-			return fmt.Errorf("error granting: %w", err)
-		}
-
-		if _, err := conn.Exec("GRANT CREATE ON SCHEMA fieldkit TO fieldkit"); err != nil {
-			return fmt.Errorf("error granting: %w", err)
-		}
-
-		if _, err := conn.Exec("SET search_path TO fieldkit, public;"); err != nil {
-			return fmt.Errorf("error granting: %w", err)
-		}
-
-		var names []string
-		if _, err := conn.Query(&names, "SELECT name FROM migrations"); err != nil {
-			log.Printf("%v", err)
-		}
-		for _, r := range names {
-			log.Printf("%s", r)
-		}
-
-		var schema []string
-		if _, err := conn.Query(&schema, "SELECT current_schema"); err != nil {
-			log.Printf("%v", err)
-		}
-		for _, r := range schema {
-			log.Printf("%s", r)
-		}
-
-		var tables []string
-		if _, err := conn.Query(&tables, "SELECT table_catalog || '.' || table_schema || '.' || table_name FROM information_schema.tables"); err != nil {
-			log.Printf("%v", err)
-		}
-		for _, r := range tables {
-			log.Printf("%s", r)
-		}
-
-		log.Printf("done creating schema...")
-
-		return nil
-	}
-
-	db := pg.Connect(options)
-
-	if err := migrations.Run(db, migrationsDir, []string{"", "migrate"}); err != nil {
-		log.Printf("migration error: %v", err)
-		return fmt.Errorf("migration error: %w", err)
 	}
 
 	return nil
 }
 
-func findMigrationsDirectory(relative string) (string, error) {
+func findMigrationsPath(relative string) (string, error) {
 	path, err := os.Getwd()
 	if err != nil {
 		return "", fmt.Errorf("unable to find migrations directory: %w", err)
