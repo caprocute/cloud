dbe2a2b9f03f9b30fcbd954b93bdbe5dc20dc293
Andrei Radoi <andrei.radoi@neurony.ro>
2025-02-03 17:52:33 +0200

    rm console log

--- portal/src/router.ts
+++ portal/src/router.ts
@@ -663,7 +663,6 @@ export default function routerFactory(store) {
 
     router.beforeEach(async (to, from, next) => {
         console.log("nav", from.name, "->", to.name);
-        console.log("ss", store);
         if (from.name === null && (to.name === null || to.name == "login")) {
             console.log("nav", "authenticated", store.getters.isAuthenticated);
             if (store.getters.isAuthenticated) {
