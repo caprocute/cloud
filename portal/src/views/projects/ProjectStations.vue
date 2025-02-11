571dd596507697b221425afe854d9e8ba582167c
Andrei Radoi <andrei.radoi@neurony.ro>
2025-02-11 02:18:43 +0200

    implement displayed pages max for pagination controls

--- portal/src/views/projects/ProjectStations.vue
+++ portal/src/views/projects/ProjectStations.vue
@@ -372,7 +372,7 @@ export default Vue.extend({
 }
 .stations-panel {
     transition: width 0.5s;
-    flex: 1;
+    flex: 0 1 38%;
     display: flex;
     flex-direction: column;
 
@@ -444,7 +444,12 @@ export default Vue.extend({
 
 .pagination {
     margin-top: auto;
+    margin-right: 0;
     padding-bottom: 1em;
+
+    ::v-deep .pages {
+        margin-top: 4px;
+    }
 }
 
 ::v-deep .station-hover-summary {
