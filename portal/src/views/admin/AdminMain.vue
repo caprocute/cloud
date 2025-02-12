67a639a45514c0e08d6aff708773987afc18b99e
Jacob Lewallen <jlewallen@gmail.com>
2025-02-11 16:11:29 -0800

    feat: Slightly prettier UI.

--- portal/src/views/admin/AdminMain.vue
+++ portal/src/views/admin/AdminMain.vue
@@ -2,17 +2,49 @@
     <StandardLayout>
         <div class="container">
             <div class="menu">
-                <router-link :to="{ name: 'adminUsers' }" class="link">Users</router-link>
-                <router-link :to="{ name: 'adminStations' }" class="link">Stations</router-link>
+                <router-link :to="{ name: 'adminUsers' }" class="link"><h2>Users</h2></router-link>
+                <router-link :to="{ name: 'adminStations' }" class="link"><h2>Stations</h2></router-link>
+            </div>
+
+            <div class="backup-upload form-container">
+                <h2>Upload Backup</h2>
+
+                <form @submit.prevent="saveForm" class="form" v-if="!busy">
+                    <div class="form-group">
+                        <input type="file" @change="upload" />
+                    </div>
+                    <div class="form-group">
+                        <button class="button-solid" type="submit">Upload</button>
+                    </div>
+                </form>
+
+                <div v-if="uploaded">
+                    <div v-if="uploaded.errors" class="form-group">
+                        <div class="label">Errors:</div>
+                        {{ uploaded.errors }}
+                    </div>
+                    <div v-if="uploaded.deviceId" class="form-group">
+                        <div class="label">Device ID:</div>
+                        {{ uploaded.deviceId }}
+                    </div>
+                    <div v-if="uploaded.generationId" class="form-group">
+                        <div class="label">Generation ID:</div>
+                        {{ uploaded.generationId }}
+                    </div>
+                    <div v-if="uploaded.deviceName" class="form-group">
+                        <div class="label">Device Name:</div>
+                        {{ uploaded.deviceName }}
+                    </div>
+                    <div v-if="uploaded.records" class="form-group">
+                        <div class="label">Records:</div>
+                        {{ uploaded.records }}
+                    </div>
+                </div>
             </div>
 
             <div class="status" v-if="status">
+                <h2>{{ $t("admin.serverLogs.heading") }}</h2>
                 <table>
-                    <thead>
-                        <tr>
-                            <th>{{ $t("admin.serverLogs.heading") }}</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         <tr>
                             <td>
@@ -55,14 +87,8 @@
                 </table>
             </div>
 
-            <div>
-                <form @submit.prevent="saveForm">
-                    <input type="file" @change="upload" />
-                    <button class="button-solid" type="submit">Upload</button>
-                </form>
-            </div>
-
             <div class="status" v-if="status">
+                <h2>Server Details</h2>
                 <table>
                     <tbody>
                         <tr>
@@ -94,6 +120,14 @@ import StandardLayout from "../StandardLayout.vue";
 import CommonComponents from "@/views/shared";
 import { PortalDeployStatus } from "@/api";
 
+type Uploaded = {
+    deviceId: string;
+    generationId: string;
+    deviceName: string;
+    errors: [string];
+    records: [number];
+};
+
 export default Vue.extend({
     name: "AdminMain",
     components: {
@@ -103,10 +137,14 @@ export default Vue.extend({
     data(): {
         status: PortalDeployStatus | null;
         form: { file: any };
+        busy: boolean;
+        uploaded: Uploaded | null;
     } {
         return {
             status: null,
             form: { file: null },
+            busy: false,
+            uploaded: null,
         };
     },
     async mounted(): Promise<void> {
@@ -122,9 +160,16 @@ export default Vue.extend({
         },
         async saveForm() {
             console.log("save-form", this.form);
-            await this.$services.api.adminUploadBackup(this.form).then(() => {
-                //
-            });
+            this.busy = true;
+            await this.$services.api
+                .adminUploadBackup(this.form)
+                .then((uploaded: any) => {
+                    console.log(uploaded);
+                    this.uploaded = uploaded;
+                })
+                .finally(() => {
+                    this.busy = false;
+                });
         },
     },
 });
@@ -155,5 +200,18 @@ export default Vue.extend({
     margin-bottom: 1em;
     font-size: 18px;
     font-weight: bold;
+}
+.backup-upload {
+    margin-top: 1em;
+    margin-bottom: 1em;
+
+    .label {
+        font-weight: bold;
+        font-size: 14pt;
+    }
+
+    .form-group {
+        margin-bottom: 1em;
+    }
 }
 </style>
