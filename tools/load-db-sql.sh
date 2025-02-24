d38692cf7e04da816926b3e001d0b0d5dcebeabc
Jacob Lewallen <jlewallen@gmail.com>
2025-02-24 15:36:24 -0800

    feat: Pg details from env, force tsdb extension version install.

--- tools/load-db-sql.sh
+++ tools/load-db-sql.sh
@@ -14,16 +14,31 @@ if [ ! -f $sql_file ]; then
 	exit 2
 fi
 
-psql -h 127.0.0.1 -p 5432 -U fieldkit postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'fieldkit';"
-psql -h 127.0.0.1 -p 5432 -U fieldkit postgres -c "DROP DATABASE fieldkit;" || true
-psql -h 127.0.0.1 -p 5432 -U fieldkit postgres -c "CREATE DATABASE fieldkit;"
+DB_HOST=$FIELDKIT_DB_HOST
+if [ -z "$DB_HOST" ]; then
+	DB_HOST=127.0.0.1
+fi
+
+DB_USER=$FIELDKIT_DB_USER
+if [ -z "$DB_HOST" ]; then
+	DB_USER=fieldkit
+fi
+
+DB_PORT=$FIELDKIT_DB_PORT
+if [ -z "$DB_PORT" ]; then
+	DB_PORT=5432
+fi
+
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'fieldkit';"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER postgres -c "DROP DATABASE fieldkit;" || true
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER postgres -c "CREATE DATABASE fieldkit;"
 
 if [ "${sql_file: -4}" == ".bz2" ]; then
-	bunzip2 -c $sql_file | psql -h 127.0.0.1 -p 5432 -U fieldkit fieldkit
+	bunzip2 -c $sql_file | psql -h $DB_HOST -p $DB_PORT -U $DB_USER fieldkit
 elif [ "${sql_file: -3}" == ".xz" ]; then
-	xz -dc $sql_file | psql -h 127.0.0.1 -p 5432 -U fieldkit fieldkit
+	xz -dc $sql_file | psql -h $DB_HOST -p $DB_PORT -U $DB_USER fieldkit
 elif [ "${sql_file: -4}" == ".sql" ]; then
-	psql -h 127.0.0.1 -p 5432 -U fieldkit fieldkit < $sql_file
+	psql -h $DB_HOST -p $DB_PORT -U $DB_USER fieldkit < $sql_file
 fi
 
 make migrate-up
