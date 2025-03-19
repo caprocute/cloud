6ab84412ae0f077e242b946d007e0f5bd90a02bb
Andrei Radoi <andrei.radoi@neurony.ro>
2025-03-19 16:55:11 +0200

    fix map-expand z-index

--- portal/src/views/projects/ProjectStations.vue
+++ portal/src/views/projects/ProjectStations.vue
@@ -464,7 +464,7 @@ export default Vue.extend({
 
 .map-expand {
     background-color: #ffffff;
-    z-index: #{$z-index-top} + 1;
+    z-index: #{$z-index-top + 1};
     position: absolute;
     right: 10px;
     top: 10px;
