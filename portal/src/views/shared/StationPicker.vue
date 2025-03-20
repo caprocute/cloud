3dc565efdddae3dbed7662c79dde4d8c22cc3ccf
Jacob Lewallen <jlewallen@gmail.com>
2025-03-20 10:37:58 -0700

    build: Upgrade typescript and nodejs.

--- portal/src/views/shared/StationPicker.vue
+++ portal/src/views/shared/StationPicker.vue
@@ -73,7 +73,7 @@ export default Vue.extend({
         },
         filter: {
             type: Function as PropType<(station: DisplayStation) => boolean>,
-            default: (_station) => true,
+            default: () => (_station) => true,
         },
     },
     data(): {
