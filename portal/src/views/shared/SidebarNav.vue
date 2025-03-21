5026011c92acc1a08ef17aa586e433a09503bdbf
Andrei Radoi <andrei.radoi@neurony.ro>
2025-03-21 18:17:49 +0200

    fix map resize on stations nav-label click

--- portal/src/views/shared/SidebarNav.vue
+++ portal/src/views/shared/SidebarNav.vue
@@ -29,7 +29,7 @@
             </div>
 
             <div class="nav-section" v-if="stations.length > 0">
-                <router-link :to="{ name: 'mapAllStations' }">
+                <router-link :to="{ name: 'mapAllStations' }" @click.native="onStationsClick">
                     <div class="nav-label">
                         <i class="icon icon-stations"></i>
                         <span v-bind:class="{ selected: viewingStations }"><StationOrSensor /></span>
@@ -147,6 +147,10 @@ export default Vue.extend({
         },
     },
     methods: {
+        onStationsClick(): void {
+            this.$emit("sidebar-toggle");
+            this.closeMenuOnMobile();
+        },
         showStation(station: DisplayStation): void {
             this.$emit("show-station", station);
             this.closeMenuOnMobile();
