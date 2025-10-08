0c4fbf4dae33b0d1a57df5e114207e7665b3674d
Andrei Radoi <andrei.radoi@neurony.ro>
2025-10-08 13:04:15 +0300

    fix z-index for project big map expand icon

--- portal/src/views/projects/ProjectStations.vue
+++ portal/src/views/projects/ProjectStations.vue
@@ -465,7 +465,7 @@ export default Vue.extend({
 
 .map-expand {
     background-color: #ffffff;
-    z-index: #{variables.$z-index-top} + 1;
+    z-index: #{variables.$z-index-top + 1};
     position: absolute;
     right: 10px;
     top: 10px;
